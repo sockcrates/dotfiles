648f8842f1bc77c0652b4d2e59acc5eafd53b8ba
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2025-03-27 20:03:29 -0400

    Clear out emacs config

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -1,17 +1,10 @@
-;; Set up package.el to work with MELPA
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
-
 (use-package doom-themes
   :ensure t
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-manegarm t)
+  (load-theme 'doom-acario-dark)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -22,29 +15,3 @@
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
-
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-(unless (package-installed-p 'org)
-  (package-install 'org))
-
-;; Load Org mode
-(require 'org)
