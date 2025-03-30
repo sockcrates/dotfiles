9098554653a5138e7d1840a9e7376f74ba2656f2
markg <17348049+sockcrates@users.noreply.github.com>
2025-03-29 21:58:39 -0400

    Offboard nvim

--- dot_config/nvim/lua/custom/plugins/git-blame.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'f-person/git-blame.nvim',
-  event = 'VeryLazy',
-  keys = {
-    { '<leader>tb', '<cmd>GitBlameToggle<CR>', desc = 'Show current line blame' },
-  },
-}
--- dot_config/nvim/lua/custom/plugins/go.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'ray-x/go.nvim',
-  config = function()
-    require('go').setup()
-  end,
-  event = { 'CmdlineEnter' },
-  ft = { 'go', 'gomod' },
-}
